e8a8048e7063292ead4b97018a0c0c43f888eccb
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-02-20 00:16:15 +0530

    Singly Linked Lists

---
+++ src/com/utkarsh/main.java
@@ -0,0 +1,10 @@
+package com.utkarsh;
+
+public class main{
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(10);
+        list.insertFirst(20);
+        list.display();
+    }
+}
