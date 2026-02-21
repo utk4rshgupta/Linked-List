49ad5b2e21a2a44e9327940101ec82383c7db075
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-02-21 16:29:14 +0530

    added more functionality

--- src/com/utkarsh/DLL.java
+++ src/com/utkarsh/DLL.java
@@ -33,6 +33,52 @@ public class DLL {
         }
         System.out.println("END");
     }
+    // add a node to the last
+    public void insertLast(int val){
+        Node node = new Node(val);
+        Node last = head;
+        node.next=null;
+        if(head==null){
+            node.prev=null;
+            head=node;
+            return;
+        }
+        while(last.next!=null){
+            last = last.next;
+        }
+        last.next=node;
+        node.prev=last;
+    }
+
+    public Node find(int value){
+        Node node = head;
+        while(node!=null) {
+            if(node.val==value) {
+            return node;
+            }
+            node = node.next;
+        }
+        return null;
+    }
+
+
+    //insert a node after a given node
+    public void insert(int after,int val){
+        Node p = find(after);
+        if(p==null){
+            System.out.println("doesn't exixt");
+        }
+        Node node = new Node(val);
+        node.next=p.next;
+        p.next=node;
+        node.prev=p;
+        if(node.next!=null){
+            node.next.prev = node;
+        }
+    }
+
+
+
     private class Node{
         int val;
         Node next;
--- src/com/utkarsh/main.java
+++ src/com/utkarsh/main.java
@@ -4,13 +4,14 @@ public class main{
     public static void main(String[] args) {
         DLL list = new DLL();
         list.insertFirst(10);
-        list.insertFirst(20);
         list.insertFirst(10);
-        list.insertFirst(20);
-        list.insertFirst(10);
-        list.insertFirst(20);
-        list.insertFirst(10);
-        list.insertFirst(20);
-        list.displayRev();
+        list.insertFirst(13);
+        list.insertFirst(24);
+        list.insertFirst(16);
+        list.insertFirst(6);
+        list.insertFirst(45);
+        list.insertFirst(98);
+        list.insert(10,99);
+        list.display();
     }
 }
