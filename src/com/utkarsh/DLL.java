9592f3fee768c68177f4638f47fa2cd8e45070a4
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-02-21 14:46:31 +0530

    Doubly Linked Lists

--- src/com/utkarsh/DLL.java
+++ src/com/utkarsh/DLL.java
@@ -1,19 +1,40 @@
 package com.utkarsh;
 
 public class DLL {
+    public void insertFirst(int val) {
+        Node node = new Node(val);
+        node.next = head;   // point new node to current head
+        node.prev = null;   // first node has no previous
 
+        if (head != null) {
+            head.prev = node; // update previous of old head
+        }
+
+        head = node; // move head to new node
+    }
+    public void display(){
+        Node node = head ;
+        while(node!=null){
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.println("END");
+    }
     private class Node{
         int val;
-        int next;
-        int prev;
+        Node next;
+        Node prev;
         public Node(int val){
             this.val=val;
         }
-        public Node(int val,int next,int prev){
+        public Node(int val,Node next,Node prev){
             this.val = val;
             this.prev = prev;
             this.next = next;
         }
-        Node head;
+
     }
+    private Node head;
+
+
 }
--- src/com/utkarsh/main.java
+++ src/com/utkarsh/main.java
@@ -2,7 +2,7 @@ package com.utkarsh;
 
 public class main{
     public static void main(String[] args) {
-        LL list = new LL();
+        DLL list = new DLL();
         list.insertFirst(10);
         list.insertFirst(20);
         list.display();
