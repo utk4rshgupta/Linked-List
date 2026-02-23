8fa0195d058efcfe7db86838bb14e6bdb72daeb6
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-02-23 14:47:54 +0530

    added more functionality

--- src/com/utkarsh/main.java
+++ src/com/utkarsh/main.java
@@ -2,16 +2,24 @@ package com.utkarsh;
 
 public class main{
     public static void main(String[] args) {
-        DLL list = new DLL();
-        list.insertFirst(10);
-        list.insertFirst(10);
-        list.insertFirst(13);
-        list.insertFirst(24);
-        list.insertFirst(16);
-        list.insertFirst(6);
-        list.insertFirst(45);
-        list.insertFirst(98);
-        list.insert(10,99);
-        list.display();
+//        DLL list = new DLL();
+//        list.insertFirst(10);
+//        list.insertFirst(10);
+//        list.insertFirst(13);
+//        list.insertFirst(24);
+//        list.insertFirst(16);
+//        list.insertFirst(6);
+//        list.insertFirst(45);
+//        list.insertFirst(98);
+//        list.insert(10,99);
+          LL list = new LL();
+          list.insertFirst(98);
+          list.insertFirst(2);
+          list.insertFirst(12);
+          list.insertFirst(23);
+          list.display();
+          list.insertRec(99,1);
+          list.display();
+
     }
 }
