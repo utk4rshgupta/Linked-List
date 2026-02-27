7ecc765b6031fe48f06b4f61de9cf7774abef61f
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-02-28 00:49:02 +0530

    added more functionality

--- src/com/utkarsh/Questions.java
+++ src/com/utkarsh/Questions.java
@@ -10,6 +10,44 @@ public class Questions {
         }
     }
 
+    // https://leetcode.com/problems/linked-list-cycle
+    // Amazon and Microsoft
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // find length of the cycle
+    public int lengthCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                // calculate the length
+                ListNode temp = slow;
+                int length = 0;
+                do {
+                    temp = temp.next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }
+        }
+        return 0;
+    }
+
     // https://leetcode.com/problems/linked-list-cycle-ii/
     public ListNode detectCycle(ListNode head) {
         LL ListNode = new LL();
@@ -46,6 +84,31 @@ public class Questions {
             s = s.next;
         }
         return s;
+    }
+
+    // Google: https://leetcode.com/problems/happy-number/
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+
+        do {
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        } while (slow != fast);
+
+        if (slow == 1) {
+            return true;
+        }
+        return false;
+    }
+    private int findSquare(int number) {
+        int ans = 0;
+        while (number > 0) {
+            int rem = number % 10 ;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
     }
 
 }
