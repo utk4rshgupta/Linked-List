59ab5e408ed7e772b8d2a7d665d823e6581b6e09
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-02-28 01:03:29 +0530

    added more functionality

--- src/com/utkarsh/Questions.java
+++ src/com/utkarsh/Questions.java
@@ -110,5 +110,16 @@ public class Questions {
         }
         return ans;
     }
+    // https://leetcode.com/problems/middle-of-the-linked-list/submissions/
+    public ListNode middleNode(ListNode head) {
+        ListNode s = head;
+        ListNode f = head;
+
+        while (f != null && f.next != null) {
+            s = s.next;
+            f = f.next.next;
+        }
+        return s;
+    }
 
 }
