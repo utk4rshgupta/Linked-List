1377fc35ee0518a6f33a0c1ed75ec09f24b342ce
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-03-01 23:16:18 +0530

    added more functionality

--- src/com/utkarsh/Questions.java
+++ src/com/utkarsh/Questions.java
@@ -124,4 +124,62 @@ public class Questions {
         return s;
     }
 
+
+    // Reversing a Linked List
+    public ListNode reverseList(ListNode head) {
+        if(head==null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode pres = head;
+        ListNode next = pres.next;
+        while(pres!=null){
+            pres.next= prev;
+            prev = pres;
+            pres=next;
+            if(next!=null){
+                next=next.next;
+            }
+        }
+        return prev;
+    }
+
+    // find reverse in an interval
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left==right){
+            return head;
+        }
+        // skip the fiorst left-1 nodes
+        ListNode curr = head;
+        ListNode prev = null;
+
+        for(int i =0;curr!=null && i<left-1;i++){
+            prev = curr;
+            curr = curr.next;
+        }
+
+        ListNode last= prev;
+        ListNode newEnd = curr;
+
+        //reverse betn left and right nodes
+
+        ListNode next = curr.next;
+        for(int i =0;curr != null && i<right-left+1;i++){
+            curr.next=prev;
+            prev  = curr;
+            curr = next;
+            if(next!=null){
+                next = next.next;
+            }
+        }
+        if(last!=null){
+            last.next= prev;
+        }else{
+            head = prev;
+        }
+        newEnd.next=curr;
+        return head;
+    }
+
+
 }
