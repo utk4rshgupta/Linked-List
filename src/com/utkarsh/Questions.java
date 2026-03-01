bb006b9a45dc29567284f5cd471f51cae306c4f0
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-03-01 23:38:44 +0530

    reorder list

--- src/com/utkarsh/Questions.java
+++ src/com/utkarsh/Questions.java
@@ -180,6 +180,28 @@ public class Questions {
         newEnd.next=curr;
         return head;
     }
+    // reorder list
+    public void reorderList(ListNode head) {
+        if(head == null || head.next==null){
+            return;
+        }
+        ListNode mid = middleNode(head);
+        ListNode hs = reverseList(mid);
+        ListNode hf = head;
 
+        while(hf!=null && hs!=null){
+            ListNode temp = hf.next;
+            hf.next=hs;
+            hf=temp;
+
+            temp = hs.next;
+            hs.next=hf;
+            hs= temp;
+
+        }
+        if(hf!=null){
+            hf.next = null;
+        }
+    }
 
 }
