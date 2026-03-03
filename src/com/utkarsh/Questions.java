359eaab63b8953e2077711b1aebc742f12e7ad29
utk4rshgupta <utkarshgupta2803@gmail.com>
2026-03-03 11:46:11 +0530

    reorder list

--- src/com/utkarsh/Questions.java
+++ src/com/utkarsh/Questions.java
@@ -204,4 +204,52 @@ public class Questions {
         }
     }
 
+
+
+    // reverse nodes in k group
+    public ListNode reverseKGroup(ListNode head,int k){
+        if(k<=1 || head == null){
+            return head;
+        }
+        ListNode curr = head;
+        ListNode prev = null;
+
+        int length = getLength(head);
+        int count = length/k;
+        while(count>0){
+          ListNode last = prev;
+          ListNode newEnd = curr;
+
+          ListNode next = curr.next;
+            for (int i = 0; curr != null && i < k; i++) {
+                curr.next = prev;
+                prev = curr;
+                curr=next;
+                if(next!=null){
+                    next=next.next;
+                }
+            }
+            if(last!=null){
+                last.next = prev;
+            }else{
+                head = prev;
+            }
+            newEnd.next = curr;
+
+            prev = newEnd;
+            count--;
+        }
+        return head;
+    }
+
+    private int getLength(ListNode head) {
+        ListNode node = head;
+        int length = 0;
+        while(node!=null){
+            length++;
+            node = node.next;
+        }
+        return length;
+    }
+
 }
